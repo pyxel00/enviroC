8c508eb5d65a72f322a39f6b7c74a0cef0833e75
pyxel00 <pyxel143@gmail.com>
2024-03-10 14:39:02 +0300

    Updated EnviroC

--- enviroc.c
+++ enviroc.c
@@ -8,53 +8,55 @@
 int main(int argc, char** argv) {
 
     FILE* main;
-    FILE* gitignore;
-    int flag;
-    char* filename = NULL;
-    char source[7] = "./src/";
-    char* fullpath = NULL;
-    char command[60 + sizeof(fullpath)] = {'\0'};;
-    bool doesGitExist = system("which git > /dev/null");
-    bool doesGccExist = system("which gcc > /dev/null");
-
-    while ((flag = getopt (argc, argv, "c:r:h")) != -1)
-    switch (flag) {
+    FILE* git_ignore;
     
+    char flag;
+    char main_code[90] = "#include <stdio.h>\n#include <stdlib.h>\nint main(int argc, char* argv) {\n\treturn 0;\n}";
+    char compile_command[] = {'\0'};
+
+    while ((flag = getopt(argc, argv, "cr:h")) != -1)
+
+    switch(flag) {
     case 'c':
-        filename = optarg;
-        fullpath = strcat(source, filename);
+        int git_exists = system("which git > /dev/null");
+        mkdir("./src", 0755);
 
-        mkdir("./src", 0700);
-        main = fopen(fullpath ,"w");
-        gitignore = fopen(".gitignore", "w");
-        fprintf(gitignore, "/output");
-        if (doesGitExist) {
-            printf("Git does not exist. Not creating a git repository\n");
-        } else {
+        main = fopen("./src/main.c", "w");
+        git_ignore = fopen("./.gitignore", "w");
+
+        fprintf(main, main_code);
+        fprintf(git_ignore, "/output");
+
+        if (git_exists == 0) {
+            printf("Found git. Commencing..\n");
             system("git init; git add --all");
-        }
-        
-        break;
-    
-    case 'r':
-        if (doesGccExist) {
-            printf("GCC does not exist.");
         } else {
-            filename = optarg;
-            fullpath = strcat(source, filename);
+            printf("Git not found..");
+            continue;
+        }
 
-            mkdir("./output", 0700);
-            sprintf(command, "gcc %s -o program; mv program ./output; ./output/program", fullpath);
-            system(command);
+        break;
+
+    case 'r':
+        int gcc_exists = system("which gcc > /dev/null");
+        mkdir("./output", 0755);
+
+        char* filepath = optarg;
+
+        if (gcc_exists == 0) {
+            sprintf(compile_command, "gcc %s -o ./output/program; ./output/program", filepath);
+            system(compile_command);
+        } else {
+            printf("GCC does not exist. Aborting.");
+            abort();
         }
         break;
-    
+
     case 'h':
-        printf("enviroC is a C environment creator and runner. It is probably written very poorly but it works!\n flags:\n -h : Display this help screen!\n -c : Takes in a file name ending in .c. Theoretically it can create all types of files but it does not work very well with others. \nIt creates a unique file unless the specified name is the same for any of the others.\n -r : Compiles and runs the specified file. Only support gcc for now\n");
+        printf("enviroC is a C environment creator and runner. It is probably written very poorly but it works!\n flags:\n -h : Display this help screen!\n -c : Creates a main.c file inside src folder.\n -r : Compiles and runs the specified file. Requires full path of the file. Only support gcc for now\n");
         break;
 
     default:
         abort();
     }
-
 }
